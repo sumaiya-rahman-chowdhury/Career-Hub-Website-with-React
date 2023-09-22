2b9bdf2e593a6ad370868746778213ced6b3e286
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-22 02:26:30 -0700

    created footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import Home from './Home.jsx';
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
     <App />
   </React.StrictMode>,
 )
