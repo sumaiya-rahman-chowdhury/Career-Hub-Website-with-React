88f32a532268150155610d9c33249b59d01de74b
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-22 07:08:26 -0700

    added 'See All Jobs' button

--- src/FeaturedJobs.jsx
+++ src/FeaturedJobs.jsx
@@ -3,11 +3,17 @@ import Job from './Job';
 
 const FeaturedJobs = () => {
     const [jobs,setJobs] = useState([]);
+
+    const [dataLength,setDataLength] = useState(4)
+
+
+
     useEffect(()=>{
         fetch('jobs.json')
         .then(res=>res.json())
         .then(data=>setJobs(data))
     },[])
+  
     return (
         <div>
               <div className=' text-center mb-6 space-y-5 mt-22'>
@@ -15,10 +21,13 @@ const FeaturedJobs = () => {
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className=' '>
-            <div className=' grid lg:grid-cols-2 gap-6 grid-cols-1'>
-                {
-                    jobs.map(job=><Job key={job.id} job={job}></Job>)
+            <div className=' grid lg:grid-cols-2 gap-6 grid-cols-1 w-11/12 mx-auto'>
+                {  
+                    jobs.slice(0,dataLength).map(job=><Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className='flex justify-center my-10'>
+                <button className='main-btn py-[19px] px-[28px]'>See All Jobs</button>
             </div>
             </div>
         </div>
--- src/Job.jsx
+++ src/Job.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import { GrLocation } from 'react-icons/gr';
 const Job = ({job}) => {
     console.log(job)
     return (
         <div>
-            <div className='space-y-4 lg:w-[500px] w-[400px] shadow-lg p-5 mx-auto my-5 rounded bg-gray-50'>
+            <div className='space-y-4 lg:w-[500px] w-full  shadow-lg p-5 mx-auto my-5 rounded bg-gray-50'>
             <img src={job.logo} alt=""/>
             <h2 className=' font-bold'>{job.job_title}</h2>
             <p>{job.company_name}</p>
@@ -12,7 +11,14 @@ const Job = ({job}) => {
                 <button className=' border-[#7E90FE] border-2 py-2 px-7 text-[#7E90FE]'>{job.remote_or_onsite}</button>
                 <button className='border-2 py-2 px-7  border-[#7E90FE] text-[#7E90FE]'>{job.job_type}</button>
             </div>
-            <p>Salary :{job.salary}</p>
+            {/*  */}
+           <div className=' flex  gap-x-3'>
+           <p className=' flex items-center' >
+            <GrLocation></GrLocation>
+            {job.location}</p>
+            <p> Salary :{job.salary}</p>
+           </div>
+           {/*  */}
             <button className='main-btn px-[18px] py-[11px]'>View Details</button>
             </div>
         </div>
