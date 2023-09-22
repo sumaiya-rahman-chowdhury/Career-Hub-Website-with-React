b091133c5ac376bec8b22f2983c0498fb755250b
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-22 05:17:33 -0700

    created job category sec + Featured Job

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='banner-scc mb-32'>
           
            <div className=' lg:flex mb-10'>
              {/* Banner description */}
-             <div className=' lg:text-left space-y-5'>
-            <h1 className='lg:text-[60px] text-[30px] font-bold'>One Step <br />Closer To Your <br /> <span className='gradient-text lg:text-[60px] text-[30px]' >Dream Job</span></h1>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+             <div className=' lg:text-left  flex-grow'>
+            <h1 className='lg:text-[60px] text-[30px] font-bold mb-6'>One Step <br />Closer To Your <br /> <span className='gradient-text lg:text-[60px] text-[30px]' >Dream Job</span></h1>
+            <p className=' mb-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
             <button className='main-btn'>Get Started</button>
             </div>
             {/* Banner Image */}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import CatList from './CatList';
+import FeaturedJobs from './FeaturedJobs';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <CatList></CatList>
+            <FeaturedJobs></FeaturedJobs>
         </div>
     );
 };
