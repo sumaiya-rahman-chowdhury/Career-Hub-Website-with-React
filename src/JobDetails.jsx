d5bc32e10a2cb70e569c9b2ae747b8e8b8cae2ae
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-22 10:47:04 -0700

    added toast

--- src/JobDetails.jsx
+++ src/JobDetails.jsx
@@ -1,15 +1,32 @@
-import { useLoaderData } from "react-router-dom";
-
+import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "./utility/LocalStorage";
 const JobDetails = () => {
     const jobs = useLoaderData();
-    console.log(jobs)
+    const { id } = useParams();
+    console.log(id)
+    const job = jobs.find(job => job.id == id)
+    console.log(job)
+    const handleApplyJob = () =>{
+        saveJobApplication(id)
+       toast('Applied Succesfully')
+    }
+    
     return (
         <div>
-            <h2>Job Details of : </h2>
-            {
-                <p>{jobs.length}</p>
-            }
-
+            <div className="grid gap-4 md:grid-cols-4">
+                <div className="border md:col-span-3">
+                    <h2 className="text-4xl">Job Details</h2>
+                    <h2>Job Details of: {job.job_title}</h2>
+                    <p>{job.company_name}</p>
+                </div>
+                <div className="border">
+                    <h2 className="text-2xl">Side things</h2>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
+                </div>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
